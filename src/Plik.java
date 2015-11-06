310311382a4e9fde196507846d76caa80782e43f
slawmalcz <slaw.malcz@wp.pl>
2015-11-06 16:36:31 +0100

    NiepelnyKonverter
    Brakuje zamiany stringow na liczby

---
+++ src/Plik.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Plik {
+	public static double [][] new_data;
+	public static ArrayList<String> data;
+	
+	 public static void main(String[] args) {
+	        String nazwaPliku = "train.csv";
+	        data = odczytajPlik(nazwaPliku);
+	        new_data=konwersja(data);
+	        zapiszPlik(nazwaPliku, new_data);
+	    }
+	 
+	    public static void zapiszPlik(String nazwaPliku, double[][] dane) {
+	        Path sciezka = Paths.get(nazwaPliku);
+	        ArrayList<String> out = new ArrayList<>();
+	        // Pobranie kolejnego "rzêdu" danych
+	        for (double[] seria : dane) {
+	            String s = Arrays.toString(seria);
+	            out.add(s);
+	        }
+	        try {
+	            Files.write(sciezka, out);
+	        } catch (IOException ex) {
+	            System.out.println("Nie mogê zapisaæ pliku!");
+	        }
+	    }
+	        
+	    public static ArrayList<String> odczytajPlik(String nazwaPliku) {
+	        Path sciezka = Paths.get(nazwaPliku);
+	        ArrayList<String> odczyt = new ArrayList<String>();
+	        try {
+	            odczyt = (ArrayList) Files.readAllLines(sciezka);
+	        } catch (IOException ex) {
+	            System.out.println("Brak pliku!");
+	        }
+	        return odczyt;
+	    }
+	 
+	    public static double [][] konwersja(ArrayList<String> dane) {
+	        // Tablica dla odczytanych danych
+	        double[][] daneOdczytane = new double[dane.size()][];
+	        int nrLinii = 0;
+	        // Pobranie kolejnych linii z listy
+	        for (String linia : dane) {
+	            // Rozbijamy liniê na poszczególne linie (przedzielone przecinkami)
+	            String[] liniaDaneString = linia.split(",");
+	            
+	            //Tutaj wstawiamy deskryptor
+	            
+	            // Tablica do przechowania danych w fomie liczb double
+	            double[] liniaDouble = new double[liniaDaneString.length];
+	            
+	            // Pêtla, która pobiera z tablicy String-ów po jednej liczbie,
+	            // konwertuje j¹ na liczbê double i zapisuje w tablicy typu double[]
+	            for (int i = 0; i < liniaDouble.length; i++) {
+	                liniaDouble[i] = Double.parseDouble(liniaDaneString[i]);
+	            }
+	            // Dodajemy tablicê z seri¹ danych do tablicy z wszystkimi danymi
+	            daneOdczytane[nrLinii] = liniaDouble;
+	            nrLinii++;
+	        }
+	        return daneOdczytane;
+	    }
+}
